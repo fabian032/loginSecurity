561434499c5729318019150ad3bd9cf049e943d4
FABIAN <FABIAN@DESKTOP-VUR1N3U>
2021-10-29 16:49:31 -0500

    prueba

---
+++ LoginSecurity/src/main/java/com/LoginSecurity/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.LoginSecurity.repository;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.LoginSecurity.entity.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>  {
+    public Optional<User> findByUsername(String username);
+}
